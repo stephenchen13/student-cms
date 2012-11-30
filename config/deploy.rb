e06f0d05cf2e903dd8d340856dfed140c6245fb6
Kevin <mcnameekm@gmail.com>
2012-11-30 15:19:42 -0500

    fixing stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ role :app, "96.8.123.73"                          # This may be the same as your
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
+# If you are using Passenger mod_raasdfasdfasils uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
